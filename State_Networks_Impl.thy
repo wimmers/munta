09f422fa69cd8e201d9d219167ae69189f85b6ff
Simon Wimmer <wimmers@in.tum.de>
2016-11-14 17:29:37 +0100

    Added prototypical instantiation for integer state variables

--- State_Networks_Impl.thy
+++ State_Networks_Impl.thy
@@ -2,6 +2,23 @@ theory State_Networks_Impl
   imports Normalized_Zone_Semantics_Impl State_Networks
 begin
 
+(* XXX Move *)
+lemma finite_lists_length_eq:
+  "finite {s. length s = r \<and> set s \<subseteq> S}" if "finite S"
+  by (rule finite_lists_length_le[OF that, THEN finite_subset[rotated], where n1 = r]) auto
+
+ (* XXX Move *)
+lemma finite_lists_boundedI:
+  assumes "\<forall> i < r. finite (S i)"
+    shows "finite {s. length s = r \<and> (\<forall>i<r. s ! i \<in> S i)}" (is "finite ?R")
+proof -
+  let ?S = "\<Union> {S i | i. i < r}"
+  have "?R \<subseteq> {s. length s = r \<and> set s \<subseteq> ?S}"
+    by (auto dest!: aux)
+  moreover have "finite \<dots>" by (rule finite_lists_length_eq) (use assms in auto)
+  ultimately show ?thesis by (rule finite_subset)
+qed
+
 abbreviation "repeat x n \<equiv> map (\<lambda> _. x) [0..<n]"
 
 subsection \<open>Pre-compiled networks with states and clocks as natural numbers\<close>
@@ -44,23 +61,26 @@ end
     "snd o (\<lambda> i. (f i, g i)) = g"
   by auto
 
-  locale Network_Reachability_Problem_precompiled = Network_Reachability_Problem_precompiled_defs +
-    assumes process_length: "length inv = p" "length trans = p" "length pred = p"
-      and processes_have_trans: "\<forall> i < p. trans ! i \<noteq> []"
-      and discrete_state_finite: "\<forall> i < p. \<forall> l < length (trans ! i). finite {s. (pred ! i ! l) s}"
-      and lengths:
-        "\<forall> i < p. length (pred ! i) = length (trans ! i) \<and> length (inv ! i) = length (trans ! i)"
-      and state_set: "\<forall> T \<in> set trans. \<forall> xs \<in> set T. \<forall> (_, _, _, _, _, l) \<in> set xs. l < length T"
-      and k_length: "length k = m + 1" -- "Zero entry is just a dummy for the zero clock"
-      (* XXX Make this an abbreviation? *)
-    assumes k_ceiling:
-      "\<forall> c \<in> {1..m}. k ! c = Max ({d. (c, d) \<in> clkp_set'} \<union> {0})" "k ! 0 = 0"
-    assumes consts_nats: "snd ` clkp_set' \<subseteq> \<nat>"
-    assumes clock_set: "clk_set' = {1..m}"
-      and p_gt_0: "p > 0"
-      and m_gt_0: "m > 0"
-      and start_has_trans: "\<forall> q < p. trans ! q ! 0 \<noteq> []" -- \<open>Necessary for refinement\<close>
-  begin
+locale Network_Reachability_Problem_precompiled_raw =
+  Network_Reachability_Problem_precompiled_defs +
+  assumes process_length: "length inv = p" "length trans = p" "length pred = p"
+    and processes_have_trans: "\<forall> i < p. trans ! i \<noteq> []"
+    and lengths:
+    "\<forall> i < p. length (pred ! i) = length (trans ! i) \<and> length (inv ! i) = length (trans ! i)"
+    and state_set: "\<forall> T \<in> set trans. \<forall> xs \<in> set T. \<forall> (_, _, _, _, _, l) \<in> set xs. l < length T"
+    and k_length: "length k = m + 1" -- "Zero entry is just a dummy for the zero clock"
+    (* XXX Make this an abbreviation? *)
+  assumes k_ceiling:
+    "\<forall> c \<in> {1..m}. k ! c = Max ({d. (c, d) \<in> clkp_set'} \<union> {0})" "k ! 0 = 0"
+  assumes consts_nats: "snd ` clkp_set' \<subseteq> \<nat>"
+  assumes clock_set: "clk_set' = {1..m}"
+    and p_gt_0: "p > 0"
+    and m_gt_0: "m > 0"
+    and start_has_trans: "\<forall> q < p. trans ! q ! 0 \<noteq> []" -- \<open>Necessary for refinement\<close>
+
+locale Network_Reachability_Problem_precompiled = Network_Reachability_Problem_precompiled_raw +
+  assumes discrete_state_finite: "\<forall> i < p. \<forall> l < length (trans ! i). finite {s. (pred ! i ! l) s}"
+begin
   
   lemma consts_nats':
     "\<forall> I \<in> set inv. \<forall> cc \<in> set I. \<forall> (c, d) \<in> collect_clock_pairs cc. d \<in> \<nat>"
@@ -253,6 +273,93 @@ lemma processes_have_trans_alt:
   
   sublocale product': Product_TA "map conv_A (fst N)" init by standard (simp add: init_states)
   
-  end (* End of locale *)
+end (* End of locale *)
+
+datatype ('c, 't) constr =
+  lt 'c 't |
+  le 'c 't |
+  eq 'c 't |
+  gt 'c 't |
+  ge 'c 't
+
+type_synonym int_var_constr = "(nat, int) cconstraint"
+
+definition check :: "int_var_constr \<Rightarrow> (nat \<Rightarrow> int) \<Rightarrow> bool" where
+  "check c x \<equiv> list_all (clock_val_a x) c"
+
+datatype ('c, 't) upd =
+  upd 'c 't |
+  inc 'c |
+  dec 'c
+
+type_synonym int_var_upd = "(nat, int) upd"
+
+fun modify :: "(nat, int) upd \<Rightarrow> int list \<Rightarrow> int list" where
+  "modify (upd i x) s = s[i := x]"
+| "modify (inc i) s = s[i := s ! i + 1]"
+| "modify (dec i) s = s[i := s ! i - 1]"
+
+locale Network_Reachability_Problem_precompiled_int_vars_defs =
+  fixes p :: nat -- "Number of processes"
+    and m :: nat -- "Number of clocks"
+    and k :: "nat list" -- "Clock ceiling. Maximal constant appearing in automaton for each state"
+    and inv :: "(nat, int) cconstraint list list" -- "Clock invariants on states per process"
+    and pred :: "int_var_constr list list" -- "Clock invariants on states per process"
+    and trans ::
+    "((nat, int) cconstraint * int_var_constr * nat act * nat list * int_var_upd * nat) list list list"
+    -- "Transitions between states per process"
+    and final :: "nat list list" -- "Final states per process. Initial location is 0"
+  fixes r :: nat -- "Number of integer variables"
+    and bounds :: "(int \<times> int) list" -- "Lower and upper bounds for the variables"
+begin
+
+  definition
+    "checkb c s \<equiv>
+    check c (op ! s) \<and> length s = r \<and> (\<forall> i < r. fst (bounds ! i) < s ! i \<and> s ! i < snd (bounds ! i))"
+
+  definition pred' where "pred' = map (map checkb) pred"
+  definition trans' where "trans' =
+    map (map (map (\<lambda> (g, c, a, r, m, l). (g, \<lambda> s. check c (op ! s), a, r, modify m, l)))) trans"
+
+end
+
+locale Network_Reachability_Problem_precompiled_int_vars =
+  Network_Reachability_Problem_precompiled_int_vars_defs p m k inv pred trans final r bounds +
+  Network_Reachability_Problem_precompiled_raw p m k inv pred' trans' final
+  for p m k inv pred trans final r bounds
+begin
+
+  lemma trans'_length:
+    "length trans' = length trans"
+    unfolding trans'_def by simp
+  
+  lemma trans'_lengths:
+    "length (trans' ! i) = length (trans ! i)" if "i < p"
+    unfolding trans'_def using process_length(2)[unfolded trans'_length] that by simp
+  
+  lemma pred'_length:
+    "length pred' = length pred"
+    unfolding pred'_def by simp
+  
+  lemma pred'_lengths:
+    "length (pred' ! i) = length (pred ! i)" if "i < p"
+    unfolding pred'_def using process_length(3)[unfolded pred'_length] that by simp
+  
+  lemma trans'_length_pred:
+    "length (trans' ! i) = length (pred ! i)" if "i < p"
+    using pred'_lengths lengths that by simp
+
+  lemma
+    "finite {s. length s = r \<and> (\<forall>i<r. fst (bounds ! i) < s ! i \<and> s ! i < snd (bounds ! i))}"
+    using finite_lists_boundedI by force
+
+  sublocale Network_Reachability_Problem_precompiled p m k inv pred' trans' final
+    apply standard
+    apply safe
+    apply (simp only: trans'_length_pred)
+    unfolding pred'_def trans'_def checkb_def
+    using process_length(3)[unfolded pred'_length] finite_lists_boundedI by force (* XXX Slow *)
+
+end
 
 end (* End of theory *)
