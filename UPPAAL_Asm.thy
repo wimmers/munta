28a6833acb9900d8d8d10465dc4192ce65af96be
Simon Wimmer <wimmers@in.tum.de>
2016-11-30 20:53:11 +0100

    Updated semantics to split updates and guards

--- UPPAAL_Asm.thy
+++ UPPAAL_Asm.thy
@@ -20,41 +20,45 @@ datatype instr =
   COPY |
   CALL |
   RETURN |
-  HALT
+  HALT |
+  STOREC nat int -- "Special instruction, signals a clock reset" |
+  SETF bool -- "Meta instruction"
 
 type_synonym stack = "int list"
 type_synonym flag = bool
-type_synonym rstate = "reg \<Rightarrow> int option" -- "Partial map from registers to values"
-type_synonym state = "addr * stack * rstate * flag"
--- "Instruction pointer, stack, register state, comparison flag"
+type_synonym rstate = "int list" -- "Partial map from registers to values"
+type_synonym state = "addr * stack * rstate * flag * nat list"
+-- "Instruction pointer, stack, register state, comparison flag, reset clocks"
 
 definition int_of :: "bool \<Rightarrow> int" where
   "int_of x \<equiv> if x then 1 else 0"
 
 fun step :: "instr \<Rightarrow> state \<Rightarrow> state option" where
-  "step (JMPZ q) (pc, st, m, f) = Some (if f then pc else q, st, m, f)" |
-  "step ADD (pc, a # b # st, m, f) = Some (pc + 1, (a + b) # st, m, f)" |
-  "step NOT (pc, b # st, m , f) =
+  "step (JMPZ q) (pc, st, m, f, rs) = Some (if f then pc else q, st, m, f, rs)" |
+  "step ADD (pc, a # b # st, m, f, rs) = Some (pc + 1, (a + b) # st, m, f, rs)" |
+  "step NOT (pc, b # st, m , f, rs) =
     (if b = 0 \<or> b = 1
-     then Some (pc + 1, int_of (b = 0) # st, m, f)
+     then Some (pc + 1, int_of (b = 0) # st, m, f, rs)
      else None)" |
-  "step AND (pc, a # b # st, m, f) =
+  "step AND (pc, a # b # st, m, f, rs) =
     (if (a = 0 \<or> a = 1) \<and> (b = 0 \<or> b = 1)
-     then Some (pc + 1, int_of (a = 1 \<and> b = 1) # st, m, f)
+     then Some (pc + 1, int_of (a = 1 \<and> b = 1) # st, m, f, rs)
      else None)" |
-  "step LE (pc, a # b # st, m, f) = Some (pc + 1, int_of (a \<le> b) # st, m, f)" |
-  "step LT (pc, a # b # st, m, f) = Some (pc + 1, int_of (a < b) # st, m, f)" |
-  "step (PUSH v) (pc, st, m, f) = Some (pc + 1, v # st, m, f)" |
-  "step POP (pc, v # st, m, f) = Some (pc + 1, st, m, f)" |
-  "step (LID r) (pc, st, m, f) =
-    (case m r of Some v \<Rightarrow> Some (pc + 1, v # st, m, f) | None \<Rightarrow> None)" |
-  "step (STORE r v) (pc, st, m, f) = Some (pc, st, m(r := Some v), f)" |
-  "step COPY (pc, st, m, f) = Some (pc + 1, int_of f # st, m, f)" |
-  "step CALL (pc, q # st, m, f) =
-    (if q \<ge> 0 then Some (nat q, int pc # st, m, f) else None)" |
-  "step RETURN (pc, q # st, m, f) =
-    (if q \<ge> 0 then Some (nat q + 1, st, m, f) else None)" |
+  "step LE (pc, a # b # st, m, f, rs) = Some (pc + 1, int_of (a \<le> b) # st, m, f, rs)" |
+  "step LT (pc, a # b # st, m, f, rs) = Some (pc + 1, int_of (a < b) # st, m, f, rs)" |
+  "step (PUSH v) (pc, st, m, f, rs) = Some (pc + 1, v # st, m, f, rs)" |
+  "step POP (pc, v # st, m, f, rs) = Some (pc + 1, st, m, f, rs)" |
+  "step (LID r) (pc, st, m, f, rs) = Some (pc + 1, m ! r # st, m, f, rs)" |
+  "step (STORE r v) (pc, st, m, f, rs) = Some (pc, st, m[r := v], f, rs)" |
+  "step COPY (pc, st, m, f, rs) = Some (pc + 1, int_of f # st, m, f, rs)" |
+  "step CALL (pc, q # st, m, f, rs) =
+    (if q \<ge> 0 then Some (nat q, int pc # st, m, f, rs) else None)" |
+  "step RETURN (pc, q # st, m, f, rs) =
+    (if q \<ge> 0 then Some (nat q + 1, st, m, f, rs) else None)" |
   "step HALT s = Some s" |
+  "step (STOREC c d) (pc, st, m, f, rs) =
+    (if d = 0 then Some (pc, st, m, f, c # rs) else None)" |
+  "step (SETF b) (pc, st, m, f, rs) = Some (pc, st, m, b, rs)" |
   "step _ _ = None"
 
 type_synonym program = "addr \<Rightarrow> instr option"
@@ -62,13 +66,13 @@ type_synonym fuel = nat
 
 fun steps :: "program \<Rightarrow> fuel \<Rightarrow> state \<Rightarrow> state option" where
   "steps _ 0 _ = None" |
-  "steps prog (Suc n) (pc, st, m, f) =
+  "steps prog (Suc n) (pc, st, m, f, rs) =
     (case prog pc of
        Some instr \<Rightarrow>
          if instr = HALT
-         then Some (pc, st, m, f)
+         then Some (pc, st, m, f, rs)
          else
-           case step instr (pc, st, m, f) of
+           case step instr (pc, st, m, f, rs) of
              Some s \<Rightarrow> steps prog n s
            | None \<Rightarrow> None
      | None \<Rightarrow> None)"
--- UPPAAL_Asm_Clocks.thy
+++ UPPAAL_Asm_Clocks.thy
@@ -7,32 +7,40 @@ abbreviation conv_cc :: "('a, int) cconstraint \<Rightarrow> ('a, real) cconstra
 
 datatype instrc =
   INSTR instr |
-  STOREC nat int |
   CEXP "(nat, int) cconstraint"
 
-type_synonym cstate = "rstate \<times> state"
-
-fun stepc :: "instrc \<Rightarrow> (nat, real) cval \<Rightarrow> cstate \<Rightarrow> cstate option" where
-  "stepc (INSTR instr) u (rs, s) =
+fun stepc :: "instrc \<Rightarrow> (nat, real) cval \<Rightarrow> state \<Rightarrow> state option" where
+  "stepc (INSTR instr) u s =
     (case step instr s of
-       Some s' \<Rightarrow> Some (rs, s')
+       Some s' \<Rightarrow> Some s'
      | None \<Rightarrow> None)" |
-  "stepc (STOREC c d) u (rs, s) = Some (rs(c := Some d), s)" |
   "stepc (CEXP cc) u (rs, pc, st, m, f) = Some (rs, pc, int_of (u \<turnstile> conv_cc cc) # st, m, f)"
 
 type_synonym programc = "addr \<Rightarrow> instrc option"
 
-fun stepsc :: "programc \<Rightarrow> fuel \<Rightarrow> (nat, real) cval \<Rightarrow> cstate \<Rightarrow> cstate option" where
+fun stepsc :: "programc \<Rightarrow> fuel \<Rightarrow> (nat, real) cval \<Rightarrow> state \<Rightarrow> state option" where
   "stepsc _ 0 _ _ = None" |
-  "stepsc prog (Suc n) u (rs, pc, st, m, f) =
+  "stepsc prog (Suc n) u (pc, st, m, f, rs) =
     (case prog pc of
        Some instr \<Rightarrow>
          if instr = INSTR HALT
-         then Some (rs, pc, st, m, f)
+         then Some (pc, st, m, f, rs)
          else
-           case stepc instr u (rs, pc, st, m, f) of
+           case stepc instr u (pc, st, m, f, rs) of
              Some s \<Rightarrow> stepsc prog n u s
            | None \<Rightarrow> None
      | None \<Rightarrow> None)"
+
+fun strip :: "instrc \<Rightarrow> instr" where
+  "strip (INSTR instr) = instr" |
+  "strip _ = HALT"
+
+fun stripf :: "instrc \<Rightarrow> instr" where
+  "stripf (INSTR instr) = instr" |
+  "stripf _ = SETF False"
+
+fun stript :: "instrc \<Rightarrow> instr" where
+  "stript (INSTR instr) = instr" |
+  "stript _ = SETF True"
 
 end
