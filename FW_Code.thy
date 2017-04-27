f4424bc795a0c86db4b75dda0d32f701e3c6e44b
Simon Wimmer <wimmers@in.tum.de>
2017-04-27 10:00:21 +0200

    Updated imperative code for new Floyd-Warshall

--- FW_Code.thy
+++ FW_Code.thy
@@ -6,38 +6,48 @@ begin
 
 definition fw_upd' :: "('a::linordered_ab_monoid_add) mtx \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> 'a mtx nres" where
   "fw_upd' m k i j =
-  RETURN (op_mtx_set m (i, j) (min (op_mtx_get m (i, j)) (op_mtx_get m (i, k) + op_mtx_get m (k, j))))"
+  RETURN (
+    op_mtx_set m (i, j) (min (op_mtx_get m (i, j)) (op_mtx_get m (i, k) + op_mtx_get m (k, j)))
+  )"
 
-definition fw' ::  "('a::linordered_ab_monoid_add) mtx \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> 'a mtx nres" where
-  "fw' m n k i j = RECT (\<lambda> fw (m, k, i, j).
-      case (k, i, j) of
-        (0, 0, 0) \<Rightarrow> fw_upd' m 0 0 0 |
-        (Suc k, 0, 0) \<Rightarrow> do {m' \<leftarrow> fw (m, k, n, n); fw_upd' m' (Suc k) 0 0} |
-        (k, Suc i, 0) \<Rightarrow> do {m' \<leftarrow> fw (m, k, i, n); fw_upd' m' k (Suc i) 0} |
-        (k, i, Suc j) \<Rightarrow> do {m' \<leftarrow> fw (m, k, i, j); fw_upd' m' k i (Suc j)}
+definition fwi' ::  "('a::linordered_ab_monoid_add) mtx \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> 'a mtx nres"
+where
+  "fwi' m n k i j = RECT (\<lambda> fw (m, k, i, j).
+      case (i, j) of
+        (0, 0) \<Rightarrow> fw_upd' m k 0 0 |
+        (Suc i, 0) \<Rightarrow> do {m' \<leftarrow> fw (m, k, i, n); fw_upd' m' k (Suc i) 0} |
+        (i, Suc j) \<Rightarrow> do {m' \<leftarrow> fw (m, k, i, j); fw_upd' m' k i (Suc j)}
     ) (m, k, i, j)"
 
-lemma fw'_simps:
-  "fw' m n 0       0       0        = fw_upd' m 0 0 0"
-  "fw' m n (Suc k) 0       0        = do {m' \<leftarrow> fw' m n k n n; fw_upd' m' (Suc k) 0 0}"
-  "fw' m n k       (Suc i) 0        = do {m' \<leftarrow> fw' m n k i n; fw_upd' m' k (Suc i) 0}"
-  "fw' m n k       i       (Suc j)  = do {m' \<leftarrow> fw' m n k i j; fw_upd' m' k i (Suc j)}"
-unfolding fw'_def by (subst RECT_unfold, (refine_mono; fail), (auto split: nat.split; fail))+
+lemma fwi'_simps:
+  "fwi' m n k 0       0        = fw_upd' m k 0 0"
+  "fwi' m n k (Suc i) 0        = do {m' \<leftarrow> fwi' m n k i n; fw_upd' m' k (Suc i) 0}"
+  "fwi' m n k i       (Suc j)  = do {m' \<leftarrow> fwi' m n k i j; fw_upd' m' k i (Suc j)}"
+unfolding fwi'_def by (subst RECT_unfold, (refine_mono; fail), (auto split: nat.split; fail))+
 
 lemma
-  "fw' m n k i j \<le> SPEC (\<lambda> r. r = uncurry (fw (curry m) n k i j))"
-by (induction "curry m" n k i j arbitrary: m rule: fw.induct)
-   (fastforce simp add: fw_upd'_def fw_upd_def upd_def fw'_simps pw_le_iff refine_pw_simps)+
+  "fwi' m n k i j \<le> SPEC (\<lambda> r. r = uncurry (fwi (curry m) n k i j))"
+by (induction "curry m" n k i j arbitrary: m rule: fwi.induct)
+   (fastforce simp add: fw_upd'_def fw_upd_def upd_def fwi'_simps pw_le_iff refine_pw_simps)+
 
 lemma fw_upd'_spec:
   "fw_upd' M k i j \<le> SPEC (\<lambda> M'. M' = uncurry (fw_upd (curry M) k i j))"
 by (auto simp: fw_upd'_def fw_upd_def upd_def pw_le_iff refine_pw_simps)
 
-lemma for_rec3_fw:
-  "for_rec3 fw_upd' M n i j k \<le> SPEC (\<lambda> M'. M' = uncurry (fw (curry M) n i j k))"
+lemma for_rec2_fwi:
+  "for_rec2 (\<lambda> M. fw_upd' M k) M n i j \<le> SPEC (\<lambda> M'. M' = uncurry (fwi (curry M) n k i j))"
 using fw_upd'_spec
-by (induction "fw_upd' :: (nat \<times> nat \<Rightarrow> 'a) \<Rightarrow> _" M n i j k rule: for_rec3.induct)
+by (induction "\<lambda> M. fw_upd' (M :: (nat \<times> nat \<Rightarrow> 'a)) k" M n i j rule: for_rec2.induct)
    (fastforce simp: pw_le_iff refine_pw_simps)+
+
+definition fw' ::  "('a::linordered_ab_monoid_add) mtx \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> 'a mtx nres" where
+  "fw' m n k = nfoldli [0..<k + 1] (\<lambda> _. True) (\<lambda> k M. for_rec2 (\<lambda> M. fw_upd' M k) M n n n) m"
+
+lemma fw'_spec:
+  "fw' m n k \<le> SPEC (\<lambda> M'. M' = uncurry (fw (curry m) n k))"
+  unfolding fw'_def
+  apply (induction k)
+  using for_rec2_fwi by (fastforce simp add: pw_le_iff refine_pw_simps curry_def)+
 
 
 context
@@ -61,7 +71,10 @@ declare fw_upd_impl.refine[sepref_fr_rules]
 sepref_register fw_upd' :: "'a i_mtx \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> 'a i_mtx nres"
 
 definition
-  "fw_impl' (M :: 'a mtx) = for_rec3 fw_upd' M n n n n"
+  "fwi_impl' (M :: 'a mtx) k = for_rec2 (\<lambda> M. fw_upd' M k) M n n n"
+
+definition
+  "fw_impl' (M :: 'a mtx) = fw' M n n"
 
 context
   notes [id_rules] = itypeI[of n "TYPE (nat)"]
@@ -70,11 +83,11 @@ begin
 
 sepref_definition fw_impl is
   "fw_impl'" :: "mtx_assn\<^sup>d \<rightarrow>\<^sub>a mtx_assn"
-unfolding fw_impl'_def[abs_def] for_rec3_eq
- apply (subst (1) nfoldli_assert'[symmetric])
- apply (subst (1) nfoldli_assert'[symmetric])
- apply (subst (1) nfoldli_assert'[symmetric])
-by sepref
+unfolding fw_impl'_def[abs_def] fw'_def for_rec2_eq by sepref
+
+sepref_definition fwi_impl is
+  "uncurry fwi_impl'" :: "[\<lambda> (_,k). k \<le> n]\<^sub>a mtx_assn\<^sup>d *\<^sub>a node_assn\<^sup>k \<rightarrow> mtx_assn"
+unfolding fwi_impl'_def[abs_def] for_rec2_eq by sepref
 
 end (* End of sepref setup *)
 
@@ -83,10 +96,10 @@ end (* End of n *)
 export_code fw_impl in SML_imp
 
 definition fw_spec where
-  "fw_spec n M \<equiv> SPEC (\<lambda> M'. 
+  "fw_spec n M \<equiv> SPEC (\<lambda> M'.
     if (\<exists> i \<le> n. M' i i < \<one>)
-    then \<not> cycle_free M n
-    else \<forall>i \<le> n. \<forall>j \<le> n. M' i j = D M i j n \<and> cycle_free M n)"
+    then \<not> cyc_free M n
+    else \<forall>i \<le> n. \<forall>j \<le> n. M' i j = D M i j n \<and> cyc_free M n)"
 
 lemma D_diag_nonnegI:
   assumes "cycle_free M n" "i \<le> n"
@@ -94,15 +107,17 @@ lemma D_diag_nonnegI:
 using assms D_dest''[OF refl, of M i i n] unfolding cycle_free_def by auto
 
 lemma fw_fw_spec:
-  "RETURN (fw M n n n n) \<le> fw_spec n M"
-unfolding fw_spec_def
+  "RETURN (FW M n) \<le> fw_spec n M"
+unfolding fw_spec_def cycle_free_diag_equiv
 proof (simp, safe, goal_cases)
   case prems: (1 i)
-  with fw_shortest_path[OF prems(3)] D_diag_nonnegI show ?case by fastforce
+  with fw_shortest_path[unfolded cycle_free_diag_equiv, OF prems(3)] D_diag_nonnegI show ?case
+    by fastforce
 next
-  case 2 then show ?case using FW_neg_cycle_detect by (force intro: fw_shortest_path[symmetric])
+  case 2 then show ?case using FW_neg_cycle_detect[unfolded cycle_free_diag_equiv]
+    by (force intro: fw_shortest_path[symmetric, unfolded cycle_free_diag_equiv])
 next
-  case 3 then show ?case using FW_neg_cycle_detect by blast
+  case 3 then show ?case using FW_neg_cycle_detect[unfolded cycle_free_diag_equiv] by blast
 qed
 
 definition
@@ -115,7 +130,7 @@ declare mtx_curry_assn_def[symmetric, fcomp_norm_unfold]
 
 lemma fw_impl'_correct:
   "(fw_impl', fw_spec) \<in> Id \<rightarrow> br curry (\<lambda> _. True) \<rightarrow> \<langle>br curry (\<lambda> _. True)\<rangle> nres_rel"
-unfolding fw_impl'_def[abs_def] using for_rec3_fw fw_fw_spec
+unfolding fw_impl'_def[abs_def] using fw'_spec fw_fw_spec
 by (fastforce simp: in_br_conv pw_le_iff refine_pw_simps intro!: nres_relI)
 
 theorem fw_impl_correct:
@@ -125,11 +140,22 @@ using fw_impl.refine[FCOMP fw_impl'_correct[THEN fun_relD, OF IdI]] .
 corollary
   "<mtx_curry_assn n M Mi> fw_impl n Mi <\<lambda> Mi'. \<exists>\<^sub>A M'. mtx_curry_assn n M' Mi' * \<up>
     (if (\<exists> i \<le> n. M' i i < \<one>)
-    then \<not> cycle_free M n
-    else \<forall>i \<le> n. \<forall>j \<le> n. M' i j = D M i j n \<and> cycle_free M n)>\<^sub>t"
-by (rule cons_rule[OF _ _ fw_impl_correct[THEN hfrefD, THEN hn_refineD]]) (sep_auto simp: fw_spec_def)+
+    then \<not> cyc_free M n
+    else \<forall>i \<le> n. \<forall>j \<le> n. M' i j = D M i j n \<and> cyc_free M n)>\<^sub>t"
+unfolding cycle_free_diag_equiv
+by (rule cons_rule[OF _ _ fw_impl_correct[THEN hfrefD, THEN hn_refineD]])
+   (sep_auto simp: fw_spec_def[unfolded cycle_free_diag_equiv])+
 
-abbreviation "FW m n \<equiv> fw m n n n n"
+definition "FWI M n k \<equiv> fwi M n k n n"
+
+definition "FWI' = uncurry ooo FWI o curry"
+
+lemma fwi_impl'_refine_FWI':
+  "(fwi_impl' n, RETURN oo PR_CONST (\<lambda> M. FWI' M n)) \<in> Id \<rightarrow> Id \<rightarrow> \<langle>Id\<rangle> nres_rel"
+unfolding fwi_impl'_def[abs_def] FWI_def[abs_def] FWI'_def using for_rec2_fwi
+by (force simp: pw_le_iff pw_nres_rel_iff refine_pw_simps)
+
+lemmas fwi_impl_refine_FWI' = fwi_impl.refine[FCOMP fwi_impl'_refine_FWI']
 
 definition "FW' = uncurry oo FW o curry"
 
@@ -137,7 +163,9 @@ definition "FW'' n M = FW' M n"
 
 lemma fw_impl'_refine_FW'':
   "(fw_impl' n, RETURN o PR_CONST (FW'' n)) \<in> Id \<rightarrow> \<langle>Id\<rangle> nres_rel"
-unfolding fw_impl'_def[abs_def] FW''_def[abs_def] FW'_def using for_rec3_fw
+unfolding fw_impl'_def[abs_def] FW''_def[abs_def] FW'_def using fw'_spec
 by (force simp: pw_le_iff pw_nres_rel_iff refine_pw_simps)
+
+lemmas fw_impl_refine_FW'' = fw_impl.refine[FCOMP fw_impl'_refine_FW'']
 
 end
