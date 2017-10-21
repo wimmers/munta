19e6c4be5da39dad08930db24478f977862b9789
Simon Wimmer <wimmers@in.tum.de>
2017-10-21 11:26:51 +0200

    Export model checker

--- Export_Checker.thy
+++ Export_Checker.thy
@@ -1,5 +1,5 @@
 theory Export_Checker
-  imports UPPAAL_State_Networks_Impl_Refine UPPAAL_State_Networks_Impl_Refine_Calc
+  imports UPPAAL_Model_Checking UPPAAL_State_Networks_Impl_Refine_Calc
 begin
 
 code_printing constant Array.new' \<rightharpoonup> (SML) "(fn/ ()/ =>/ Array.array/
@@ -57,6 +57,7 @@ definition
   "bounded_int bounds (s :: int list) \<equiv>
    length s = length bounds \<and> (\<forall> i < length s. fst (bounds ! i) < s ! i \<and> s ! i < snd (bounds ! i))"
 
+(*
 export_code
   check_and_verify init_pred_check time_indep_check1 time_indep_check2 conjunction_check2
   LT LE EQ GE GT integer_of_nat nat_of_integer int_of_integer integer_of_int
@@ -72,12 +73,33 @@ export_code
   REACHABLE UNREACHABLE INIT_INV_ERR
   UPPAAL_Reachability_Problem_precompiled_defs.k
   in SML_imp module_name Reachability_Checker
-  file "ML/UPPAAL_Reachability_Checker.sml"
+file "ML/UPPAAL_Reachability_Checker.sml"
+*)
 
+(*
 ML_file_debug "ML/UPPAAL_Reachability_Checker.sml"
 
 ML \<open>
   open Reachability_Checker;;
 \<close>
+*)
+
+    
+export_code
+  precond_mc Pure.type init_pred_check time_indep_check1 time_indep_check2 conjunction_check2
+  LT LE EQ GE GT integer_of_nat nat_of_integer int_of_integer integer_of_int
+  In INSTR loc
+  UPPAAL_Reachability_Problem_precompiled_defs.check_pre
+  map_option
+  UPPAAL_Reachability_Problem_precompiled_start_state
+  UPPAAL_Reachability_Problem_precompiled
+  UPPAAL_Reachability_Problem_precompiled_start_state_axioms
+  UPPAAL_Reachability_Problem_precompiled_defs.check_ceiling
+  bounded_int
+  nat
+  REACHABLE UNREACHABLE INIT_INV_ERR
+  UPPAAL_Reachability_Problem_precompiled_defs.k
+  in SML_imp module_name Model_Checker
+  file "ML/UPPAAL_Model_Checker.sml"
 
 end (* End of Theory *)
