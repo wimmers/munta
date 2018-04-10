d315acf80aa88f284abec74a5fbca7cc39a9e1ab
Simon Wimmer <wimmers@in.tum.de>
2018-04-10 15:32:12 +0200

    Server script

---
+++ ML/server.py
@@ -0,0 +1,33 @@
+import BaseHTTPServer
+import subprocess
+
+MUNTA_PATH = "./munta"
+PORT = 3069
+
+def run_munta(query):
+    p = subprocess.Popen([MUNTA_PATH], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    return p.communicate(input=query)
+
+class handler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body>GET is not supported</body></html>")
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        self._set_headers()
+        self.wfile.write(run_munta(post_data)[0])
+
+def run(server_class=BaseHTTPServer.HTTPServer, handler_class=handler):
+    server_address = ('', PORT)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+run()
