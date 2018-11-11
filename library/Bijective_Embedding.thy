dff019029c87f9981a575df0a1cc73214b1a6a29
Simon Wimmer <wimmers@in.tum.de>
2018-11-11 18:50:04 +0100

    Close sorries

--- library/Bijective_Embedding.thy
+++ library/Bijective_Embedding.thy
@@ -17,6 +17,190 @@ theorem card_vimage_inj_on:
   by (auto 4 3 simp: subset_image_iff inj_on_vimage_image_eq
       intro: card_image[symmetric, OF subset_inj_on])
 
+context
+begin
+
+private lemma finiteI: "finite {x. x \<in> A \<and> ordX x \<le> ordX a \<and> Q x}" (is "finite ?S")
+    if "inj_on ordX A" for A a Q and ordX :: "_ \<Rightarrow> nat"
+  proof -
+    have "?S \<subseteq> {x. x \<in> A \<and> ordX x \<le> ordX a}"
+      by blast
+    moreover have "finite \<dots>"
+    proof -
+      have "\<dots> = ordX -` {n. n \<le> ordX a} \<inter> A"
+        by auto
+      moreover from \<open>inj_on ordX A\<close> have "finite \<dots>"
+        by (intro finite_vimage_IntI) auto
+      ultimately show ?thesis
+        by simp
+    qed
+    ultimately show ?thesis
+      by (rule finite_subset)
+  qed
+
+qualified lemma enumeration_skip_finite_set_surj:
+  "\<exists>a \<in> A. a \<notin> S \<and> card {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> S} = n"
+  if "n > 0" "inj_on ordX A" "ordX ` A = \<nat>" "finite S" "S \<subseteq> A" for ordX :: "_ \<Rightarrow> nat"
+  using \<open>finite S\<close> \<open>n > 0\<close> \<open>S \<subseteq> A\<close>
+proof (induction arbitrary: n)
+  case empty
+  let ?a = "the_inv_into A ordX (n - 1)"
+  have "card {x \<in> A. ordX x \<le> n - 1} = n"
+  proof -
+    have "{x \<in> A. ordX x \<le> n - 1} = ordX -` {x. x \<le> n - 1} \<inter> A"
+      by auto
+    moreover have "card \<dots> = card {x. x \<le> n - 1}"
+      using \<open>inj_on ordX _\<close> \<open>ordX ` A = \<nat>\<close> by (intro card_vimage_inj_on) (auto simp: Nats_def)
+    moreover have "\<dots> = n"
+      using \<open>n > 0\<close> by auto
+    ultimately show ?thesis
+      by auto
+  qed
+  then have "\<exists> a \<in> A. card {x \<in> A. ordX x \<le> ordX a} = n"
+    using \<open>inj_on ordX A\<close> \<open>ordX ` A = \<nat>\<close>
+    unfolding Nats_def
+    by (intro bexI[where x = "the_inv_into A ordX (n - 1)"])
+       (auto intro: the_inv_into_into simp: f_the_inv_into_f[where f = ordX])
+  then show ?case
+    by simp
+next
+  case (insert x F)
+  then obtain a where a: "a \<in> A" "a \<notin> F" "card {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F} = n"
+    by blast
+  show ?case
+  proof (cases "ordX x \<le> ordX a")
+    case True
+    then have *:
+      "{xa \<in> A. ordX xa \<le> ordX a \<and> xa \<notin> insert x F} = {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F} - {x}"
+      by auto
+    let ?m = "Max (ordX ` F)"
+    show ?thesis
+    proof (cases "\<exists>b \<in> A. b \<notin> F \<and> ordX a < ordX b \<and> ordX b \<le> ?m")
+      case False
+      let ?a = "the_inv_into A ordX (max ?m (ordX a) + 1)"
+      let ?S = "{xa \<in> A. ordX xa \<le> ordX ?a \<and> xa \<notin> insert x F}"
+      have "?a \<in> A"
+        by (simp add: Nats_def \<open>inj_on ordX A\<close> \<open>ordX ` A = \<nat>\<close> the_inv_into_into)
+      have "ordX ?a = max ?m (ordX a) + 1"
+        using \<open>inj_on ordX A\<close> \<open>ordX ` A = \<nat>\<close> unfolding Nats_def
+        by (subst f_the_inv_into_f[where f = ordX]) auto
+      then have "?S = {xa \<in> A. ordX xa \<le> max ?m (ordX a) + 1 \<and> xa \<notin> insert x F}"
+        by simp
+      also have "\<dots> = {xa \<in> A. ordX xa \<le> ordX a \<and> xa \<notin> insert x F} \<union> {?a}"
+      proof -
+        have "{xa \<in> A. ordX xa = max ?m (ordX a) + 1 \<and> xa \<notin> insert x F} = {?a}"
+          unfolding \<open>ordX ?a = _\<close>
+          apply (auto simp: \<open>inj_on ordX A\<close> the_inv_into_f_eq)
+          using True \<open>ordX ?a = _\<close> \<open>?a \<in> A\<close>
+          apply -
+          apply (auto; fail)+
+          by (metis Max.in_idem Suc_eq_plus1 discrete finite_imageI imageI less_not_refl insert(1)
+                    max.cobounded2 max.commute max.strict_coboundedI1)
+        have *: "{xa \<in> A. ordX xa \<le> ordX a \<and> xa \<notin> insert x F}
+              = {xa \<in> A. ordX xa \<le> max ?m (ordX a) \<and> xa \<notin> insert x F}"
+          using False by auto
+        show ?thesis
+          unfolding \<open>_ = {?a}\<close>[symmetric] * by auto
+      qed
+      finally have "?S = ({x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F} - {x}) \<union> {?a}"
+        by auto
+      moreover have "?a \<noteq> x"
+        using True \<open>ordX ?a = max (MAXIMUM F ordX) (ordX a) + 1\<close> by auto
+      moreover have "?a \<notin> {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F}"
+        using \<open>ordX ?a = max (MAXIMUM F ordX) (ordX a) + 1\<close> by auto
+      moreover have "x \<in> {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F}"
+        using \<open>ordX x \<le> _\<close> \<open>insert x _ \<subseteq> A\<close> \<open>x \<notin> F\<close> by auto
+      ultimately have "card ?S = card {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F}"
+        apply simp
+        apply (subst card_insert)
+        subgoal
+          by (force intro: finiteI[OF \<open>inj_on ordX A\<close>, of a])
+        apply simp
+        apply (subst card.remove[symmetric])
+        subgoal
+          by (force intro: finiteI[OF \<open>inj_on ordX A\<close>, of a])
+        by auto
+      also have "\<dots> = n"
+        using \<open>_ = n\<close> by auto
+      finally show ?thesis
+        using \<open>inj_on ordX A\<close> \<open>ordX ` A = \<nat>\<close> unfolding Nats_def
+        apply -
+        apply (rule bexI[where x = ?a])
+         apply (subst f_the_inv_into_f[where f = ordX])
+           apply force+
+         apply (auto intro: the_inv_into_into)
+        using \<open>ordX x \<le> _\<close>
+          apply (subgoal_tac "ordX x = Suc (ordX a)"; simp add: f_the_inv_into_f[where f = ordX])
+         apply (subgoal_tac "ordX ?a \<le> ?m")
+        subgoal
+          using False
+          apply (simp add: f_the_inv_into_f[where f = ordX])
+          done
+         apply (rule Max_ge)
+        using \<open>finite F\<close> apply (rule finite_imageI)
+         apply (rule imageI)
+         apply (simp add: f_the_inv_into_f[where f = ordX])
+        using \<open>ordX ?a = _\<close> by simp
+    next
+      case True
+      let ?M = "{b \<in> A. ordX a < ordX b \<and> ordX b \<le> ?m \<and> b \<notin> F}"
+      from True have "?M \<noteq> {}"
+        by auto
+      have "finite ?M"
+      proof -
+        have "?M \<subseteq> {b \<in> A. ordX b \<le> ?m}"
+          by auto
+        moreover have "finite \<dots>"
+        proof -
+          have *: "\<dots> = ordX -` {x. x \<le> ?m} \<inter> A"
+            by auto
+          from \<open>inj_on ordX A\<close> show ?thesis
+            unfolding * by (intro finite_vimage_IntI) auto
+        qed
+        ultimately show ?thesis
+          by (rule finite_subset)
+      qed
+      let ?a = "arg_min_on ordX ?M"
+      from arg_min_if_finite[OF \<open>finite ?M\<close> \<open>?M \<noteq> {}\<close>, of ordX] have a:
+        "?a \<in> ?M" "\<not> (\<exists> x \<in> ?M. ordX x < ordX ?a)"
+        by fast+
+      with \<open>ordX x \<le> ordX a\<close> have "?a \<noteq> x"
+        by auto
+      then have **: "{xa \<in> A. ordX xa \<le> ordX ?a \<and> xa \<noteq> x \<and> xa \<notin> F} =
+            {xa \<in> A. ordX xa \<le> ordX a \<and> xa \<noteq> x \<and> xa \<notin> F} \<union> {?a}"
+        using a
+        by auto
+           (smt \<open>inj_on ordX A\<close> inj_on_eq_iff le_eq_less_or_eq le_trans mem_Collect_eq not_le)
+      have "?a \<notin> {xa \<in> A. ordX xa \<le> ordX a \<and> xa \<noteq> x \<and> xa \<notin> F}"
+        using a(1) by auto
+      then have "card {xa \<in> A. ordX xa \<le> ordX ?a \<and> xa \<noteq> x \<and> xa \<notin> F} = n"
+        unfolding *[simplified] **
+        using \<open>card _ = n\<close>
+        apply simp
+        apply (subst card_insert)
+        subgoal
+          by (auto intro: finiteI[OF \<open>inj_on ordX A\<close>])
+        apply simp
+        apply (subst card.remove[symmetric])
+        subgoal
+          by (auto intro: finiteI[OF \<open>inj_on ordX A\<close>])
+        subgoal
+          using \<open>insert x F \<subseteq> A\<close> \<open>ordX x \<le> _\<close> \<open>x \<notin> F\<close> by simp
+        apply assumption
+        done
+      then show ?thesis
+        using a(1) \<open>ordX x \<le> _\<close> by (intro bexI[where x = ?a]) auto
+    qed
+  next
+    case False
+    then have
+      "{xa \<in> A. ordX xa \<le> ordX a \<and> xa \<notin> insert x F} = {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F}"
+      by auto
+    with a False show ?thesis
+      by (intro bexI[where x = a]) auto
+  qed
+qed
+
 lemma bijective_embedding:
   fixes f :: "'a \<Rightarrow> 'b"
     and A :: "'a set" and B :: "'b set"
@@ -35,189 +219,21 @@ proof -
   define P where "P a b \<equiv> a \<in> A \<and> b \<in> B \<and> a \<notin> S \<and> b \<notin> f ` S \<and>
     card {x. x \<in> A \<and> ordX x \<le> ordX a \<and> x \<notin> S} = card {x. x \<in> B \<and> ordY x \<le> ordY b \<and> x \<notin> f ` S}"
     for a b
-  have finiteI: "finite {x. x \<in> A \<and> ordX x \<le> ordX a \<and> Q x}" (is "finite ?S")
-    if "inj_on ordX A" for A a Q and ordX :: "_ \<Rightarrow> nat"
-  proof -
-    have "?S \<subseteq> {x. x \<in> A \<and> ordX x \<le> ordX a}"
-      by blast
-    moreover have "finite \<dots>"
-    proof -
-      have "\<dots> = ordX -` {n. n \<le> ordX a} \<inter> A"
-        by auto
-      moreover from \<open>inj_on ordX A\<close> have "finite \<dots>"
-        by (intro finite_vimage_IntI) auto
-      ultimately show ?thesis
-        by simp
-    qed
-    ultimately show ?thesis
-      by (rule finite_subset)
-  qed
   have P_right: "\<exists> b. P a b" if "a \<in> A" "a \<notin> S" for a
-    sorry
+  proof -
+    have *: "\<exists>b \<in> B. b \<notin> f ` S \<and> card {x \<in> B. ordY x \<le> ordY b \<and> x \<notin> f ` S} = n" if "n > 0" for n
+      using \<open>n > 0\<close> \<open>finite S\<close> \<open>f ` S \<subseteq> B\<close> \<open>inj_on ordY B\<close> \<open>ordY ` B = \<nat>\<close>
+      by (intro enumeration_skip_finite_set_surj) auto
+    from that have "card {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> S} > 0"
+      unfolding card_gt_0_iff by (auto intro: finiteI[OF \<open>inj_on ordX A\<close>])
+    from *[OF this] show ?thesis
+      unfolding P_def using that by auto
+  qed
   have P_left: "\<exists>a. P a b" if "b \<in> B" "b \<notin> f ` S" for b
   proof -
     have "\<exists>a \<in> A. a \<notin> S \<and> card {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> S} = n" if "n > 0" for n
-      using \<open>finite S\<close> \<open>n > 0\<close> \<open>S \<subseteq> A\<close>
-    proof (induction arbitrary: n)
-      case empty
-      let ?a = "the_inv_into A ordX (n - 1)"
-      have "card {x \<in> A. ordX x \<le> n - 1} = n"
-      proof -
-        have "{x \<in> A. ordX x \<le> n - 1} = ordX -` {x. x \<le> n - 1} \<inter> A"
-          by auto
-        moreover have "card \<dots> = card {x. x \<le> n - 1}"
-          using \<open>inj_on ordX _\<close> \<open>ordX ` A = \<nat>\<close> by (intro card_vimage_inj_on) (auto simp: Nats_def)
-        moreover have "\<dots> = n"
-          using \<open>n > 0\<close> by auto
-        ultimately show ?thesis
-          by auto
-      qed
-      then have "\<exists> a \<in> A. card {x \<in> A. ordX x \<le> ordX a} = n"
-        using \<open>inj_on ordX A\<close> \<open>ordX ` A = \<nat>\<close>
-        unfolding Nats_def
-        by (intro bexI[where x = "the_inv_into A ordX (n - 1)"])
-          (auto intro: the_inv_into_into simp: f_the_inv_into_f[where f = ordX])
-      then show ?case
-        by simp
-    next
-      case (insert x F)
-      then obtain a where a: "a \<in> A" "a \<notin> F" "card {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F} = n"
-        by blast
-      show ?case
-      proof (cases "ordX x \<le> ordX a")
-        case True
-        then have *:
-          "{xa \<in> A. ordX xa \<le> ordX a \<and> xa \<notin> insert x F} = {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F} - {x}"
-          by auto
-        let ?m = "Max (ordX ` F)"
-        show ?thesis
-        proof (cases "\<exists>b \<in> A. b \<notin> F \<and> ordX a < ordX b \<and> ordX b \<le> ?m")
-          case False
-          let ?a = "the_inv_into A ordX (max ?m (ordX a) + 1)"
-          let ?S = "{xa \<in> A. ordX xa \<le> ordX ?a \<and> xa \<notin> insert x F}"
-          have "?a \<in> A"
-            by (simp add: Nats_def \<open>inj_on ordX A\<close> \<open>ordX ` A = \<nat>\<close> the_inv_into_into)
-          have "ordX ?a = max ?m (ordX a) + 1"
-            using \<open>inj_on ordX A\<close> \<open>ordX ` A = \<nat>\<close> unfolding Nats_def
-            by (subst f_the_inv_into_f[where f = ordX]) auto
-          then have "?S = {xa \<in> A. ordX xa \<le> max ?m (ordX a) + 1 \<and> xa \<notin> insert x F}"
-            by simp
-          also have "\<dots> = {xa \<in> A. ordX xa \<le> ordX a \<and> xa \<notin> insert x F} \<union> {?a}"
-          proof -
-            have "{xa \<in> A. ordX xa = max ?m (ordX a) + 1 \<and> xa \<notin> insert x F} = {?a}"
-              unfolding \<open>ordX ?a = _\<close>
-              apply (auto simp add: \<open>inj_on ordX A\<close> the_inv_into_f_eq)
-              using True \<open>ordX ?a = _\<close> \<open>?a \<in> A\<close>
-                 apply auto
-              by (smt Max.in_idem One_nat_def \<open>inj_on ordX A\<close> add.right_neutral add_Suc_right
-                  discrete finite_imageI inj_on_image_mem_iff insert.prems(2) insert_subset
-                  insert(1) max_def not_less not_less_iff_gr_or_eq)
-            have *: "{xa \<in> A. ordX xa \<le> ordX a \<and> xa \<notin> insert x F}
-              = {xa \<in> A. ordX xa \<le> max ?m (ordX a) \<and> xa \<notin> insert x F}"
-              using False by auto
-            show ?thesis
-              unfolding \<open>_ = {?a}\<close>[symmetric] * by auto
-          qed
-          finally have "?S = ({x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F} - {x}) \<union> {?a}"
-            by auto
-          moreover have "?a \<noteq> x"
-            using True \<open>ordX ?a = max (MAXIMUM F ordX) (ordX a) + 1\<close> by auto
-          moreover have "?a \<notin> {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F}"
-            using \<open>ordX ?a = max (MAXIMUM F ordX) (ordX a) + 1\<close> by auto
-          moreover have "x \<in> {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F}"
-            using \<open>ordX x \<le> _\<close> \<open>insert x _ \<subseteq> A\<close> \<open>x \<notin> F\<close> by auto
-          ultimately have "card ?S = card {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F}"
-            apply simp
-            apply (subst card_insert)
-            subgoal
-              by (force intro: finiteI[OF \<open>inj_on ordX A\<close>, of a])
-            apply simp
-            apply (subst card.remove[symmetric])
-            subgoal
-              by (force intro: finiteI[OF \<open>inj_on ordX A\<close>, of a])
-            by auto
-          also have "\<dots> = n"
-            using \<open>_ = n\<close> by auto
-          finally show ?thesis
-            using \<open>inj_on ordX A\<close> \<open>ordX ` A = \<nat>\<close> unfolding Nats_def
-            apply -
-            apply (rule bexI[where x = ?a])
-             apply (subst f_the_inv_into_f[where f = ordX])
-               apply force+
-             apply (auto intro: the_inv_into_into)
-            using \<open>ordX x \<le> _\<close>
-              apply (subgoal_tac "ordX x = Suc (ordX a)"; simp add: f_the_inv_into_f[where f = ordX])
-             apply (subgoal_tac "ordX ?a \<le> ?m")
-            subgoal
-              using False
-              apply (simp add: f_the_inv_into_f[where f = ordX])
-              done
-             apply (rule Max_ge)
-            using \<open>finite F\<close> apply (rule finite_imageI)
-             apply (rule imageI)
-             apply (simp add: f_the_inv_into_f[where f = ordX])
-            using \<open>ordX ?a = _\<close> by simp
-        next
-          case True
-          let ?M = "{b \<in> A. ordX a < ordX b \<and> ordX b \<le> ?m \<and> b \<notin> F}"
-          from True have "?M \<noteq> {}"
-            by auto
-          have "finite ?M"
-          proof -
-            have "?M \<subseteq> {b \<in> A. ordX b \<le> ?m}"
-              by auto
-            moreover have "finite \<dots>"
-            proof -
-              have *: "\<dots> = ordX -` {x. x \<le> ?m} \<inter> A"
-                by auto
-              from \<open>inj_on ordX A\<close> show ?thesis
-                unfolding * by (intro finite_vimage_IntI) auto
-            qed
-            ultimately show ?thesis
-              by (rule finite_subset)
-          qed
-          let ?a = "arg_min_on ordX ?M"
-          from arg_min_if_finite[OF \<open>finite ?M\<close> \<open>?M \<noteq> {}\<close>, of ordX] have a:
-            "?a \<in> ?M" "\<not> (\<exists> x \<in> ?M. ordX x < ordX ?a)"
-            by fast+
-          with \<open>ordX x \<le> ordX a\<close> have "?a \<noteq> x"
-            by auto
-          then have **: "{xa \<in> A. ordX xa \<le> ordX ?a \<and> xa \<noteq> x \<and> xa \<notin> F} =
-            {xa \<in> A. ordX xa \<le> ordX a \<and> xa \<noteq> x \<and> xa \<notin> F} \<union> {?a}"
-            using a(1)
-            apply auto
-            subgoal
-              by (smt \<open>inj_on ordX A\<close> a inj_on_eq_iff le_eq_less_or_eq le_trans mem_Collect_eq not_le)
-            done
-          have "?a \<notin> {xa \<in> A. ordX xa \<le> ordX a \<and> xa \<noteq> x \<and> xa \<notin> F}"
-            using a(1) by auto
-          then have "card {xa \<in> A. ordX xa \<le> ordX ?a \<and> xa \<noteq> x \<and> xa \<notin> F} = n"
-            unfolding *[simplified] **
-            using \<open>card _ = n\<close>
-            apply simp
-            apply (subst card_insert)
-            subgoal
-              by (auto intro: finiteI[OF \<open>inj_on ordX A\<close>])
-            apply simp
-            apply (subst card.remove[symmetric])
-            subgoal
-              by (auto intro: finiteI[OF \<open>inj_on ordX A\<close>])
-            subgoal
-              using \<open>insert x F \<subseteq> A\<close> \<open>ordX x \<le> _\<close> \<open>x \<notin> F\<close> by simp
-            apply assumption
-            done
-          then show ?thesis
-            using a(1) \<open>ordX x \<le> _\<close> by (intro bexI[where x = ?a]) auto
-        qed
-      next
-        case False
-        then have
-          "{xa \<in> A. ordX xa \<le> ordX a \<and> xa \<notin> insert x F} = {x \<in> A. ordX x \<le> ordX a \<and> x \<notin> F}"
-          by auto
-        with a False show ?thesis
-          by (intro bexI[where x = a]) auto
-      qed
-    qed
+      using \<open>n > 0\<close> \<open>finite S\<close> \<open>S \<subseteq> A\<close> \<open>inj_on ordX A\<close> \<open>ordX ` A = \<nat>\<close>
+      by (intro enumeration_skip_finite_set_surj)
     moreover from that have "card {x \<in> B. ordY x \<le> ordY b \<and> x \<notin> f ` S} > 0"
       unfolding card_gt_0_iff by (auto intro: finiteI[OF \<open>inj_on ordY B\<close>])
     ultimately show ?thesis
@@ -362,7 +378,7 @@ proof -
       apply (cases "y \<in> f ` S")
       subgoal
         by (rule bexI[where x = "the_inv_into S f y"])
-          (auto simp:
+           (auto simp:
             h_def \<open>inj_on f S\<close> assms(2) the_inv_into_f_eq the_inv_into_f_f the_inv_into_into
             )
       subgoal
@@ -380,6 +396,8 @@ proof -
   ultimately show ?thesis
     by (intro exI conjI)
 qed
+
+end
 
 definition extend_bij :: "('a :: countable \<Rightarrow> 'b :: countable) \<Rightarrow> 'a set \<Rightarrow> _" where
   "extend_bij f S \<equiv> SOME h. bij h \<and> (\<forall>x \<in> S. h x = f x)"
