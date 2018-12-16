e88fef405edf084b9d8aeb3cca83b00ef495838e
Simon Wimmer <wimmers@in.tum.de>
2018-12-16 17:06:40 +0100

    Generalized deadlock checker

--- Deadlock_Checking.thy
+++ Deadlock_Checking.thy
@@ -5,52 +5,6 @@ begin
 context UPPAAL_Reachability_Problem_precompiled'
 begin
 
-definition deadlock_checker where
-  "deadlock_checker \<equiv>
-    let
-      key = return \<circ> fst;
-      sub = impl.subsumes_impl;
-      copy = impl.state_copy_impl;
-      start = impl.a\<^sub>0_impl;
-      final = (\<lambda>_. return False);
-      succs = impl.succs_impl;
-      empty = impl.emptiness_check_impl;
-      P = impl.check_deadlock_neg_impl;
-      trace = impl.tracei
-    in do {
-      r1 \<leftarrow> impl.is_start_in_states_impl;
-      if r1 then do {
-        r2 \<leftarrow> check_passed_impl succs start final sub empty key copy trace P;
-        return r2
-      }
-      else return True
-    }
-    "
-
-thm impl.check_passed_impl_hnr deadlock_start_iff
-thm impl.check_passed_impl_start_hnr impl.check_passed_impl_start_def
-
-interpretation ta_bisim: Bisimulation_Invariant
-    "(\<lambda>(l, u) (l', u'). conv_A A \<turnstile>' \<langle>l, u\<rangle> \<rightarrow> \<langle>l', u'\<rangle>)"
-    "(\<lambda>(l, u) (l', u'). conv_A A \<turnstile>' \<langle>l, u\<rangle> \<rightarrow> \<langle>l', u'\<rangle>)"
-    "(\<lambda>(l, u) (l', u'). l' = l \<and> (\<forall> c. c \<in> clk_set (conv_A A) \<longrightarrow> u c = u' c))"
-    "(\<lambda>_. True)" "(\<lambda>_. True)"
-    by (rule ta_bisimulation[of "conv_A A"])
-
-thm ta_bisim.deadlock_iff[of _ "((init, s\<^sub>0), u)"]
-thm clocks_I[of u "\<lambda>_. 0"]
-
-lemma deadlock_zero_clock_val_iff:
-  "(\<exists>u\<^sub>0. (\<forall>c\<in>{1..m}. u\<^sub>0 c = 0) \<and> deadlock ((init, s\<^sub>0), u\<^sub>0)) \<longleftrightarrow> deadlock ((init, s\<^sub>0), \<lambda>_. 0)"
-  apply safe
-  subgoal for u
-    by (subst ta_bisim.deadlock_iff, drule clocks_I, auto)
-  by auto
-
-thm start_states' states_states' start_states'[THEN states'_states']
-
-thm Normalized_Zone_Semantics_Impl_Refine.state_set_def product'.all_prop_start
-
 text \<open>
   \<^item> @{term equiv.defs.states'}
   \<^item> @{term EA} is @{term equiv.state_ta},
@@ -68,34 +22,18 @@ lemma F_is_FalseI:
   shows "PR_CONST (\<lambda>(x, y). F x y) = (\<lambda>_. False)"
   unfolding F_def by (subst formula_is_false) auto
 
-thm impl.check_passed_impl_hnr[unfolded deadlock_zero_clock_val_iff, folded deadlock_start_iff has_deadlock_def, OF _ F_is_FalseI]
-
 lemma final_fun_is_False:
   "final_fun a = False"
   unfolding final_fun_def by (subst formula_is_false) auto
 
-lemma F_impl_is_False:
-  "impl.F_impl = (\<lambda>_. return False)"
-  unfolding impl.F_impl_def final_fun_is_False by simp
-
-lemma deadlock_checker_correct:
-  "(uncurry0 deadlock_checker, uncurry0 (Refine_Basic.RETURN (deadlock ((init, s\<^sub>0), \<lambda>_. 0))))
-  \<in> unit_assn\<^sup>k \<rightarrow>\<^sub>a bool_assn"
-  unfolding
-    deadlock_checker_def Let_def F_impl_is_False[symmetric]
-    impl.check_passed_impl_start_def[symmetric] deadlock_zero_clock_val_iff[symmetric]
-  using impl.check_passed_impl_start_hnr[OF F_is_FalseI] .
-
-lemmas deadlock_checker_hoare = deadlock_checker_correct[
-  to_hnr, unfolded hn_refine_def, simplified,
-  folded deadlock_start_iff has_deadlock_def
-]
+lemmas deadlock_checker_hoare = impl.deadlock_checker_hoare[
+    OF final_fun_is_False F_is_FalseI, folded deadlock_start_iff has_deadlock_def]
 
 end
 
 schematic_goal deadlock_checker_alt_def:
-  "deadlock_checker \<equiv> ?impl"
-  unfolding deadlock_checker_def impl.succs_impl_def
+  "impl.deadlock_checker \<equiv> ?impl"
+  unfolding impl.deadlock_checker_def impl.succs_impl_def
   unfolding impl.E_op''_impl_def impl.abstr_repair_impl_def impl.abstra_repair_impl_def
   unfolding
     impl.start_inv_check_impl_def impl.unbounded_dbm_impl_def
@@ -114,7 +52,7 @@ schematic_goal deadlock_checker_alt_def:
   by (rule Pure.reflexive)
 
 schematic_goal deadlock_checker_alt_def_refined:
-  "deadlock_checker \<equiv> ?impl"
+  "impl.deadlock_checker \<equiv> ?impl"
   unfolding deadlock_checker_alt_def
   unfolding fw_impl'_int
   unfolding inv_fun_def trans_fun_def trans_s_fun_def trans_i_fun_def
@@ -176,7 +114,9 @@ proof -
       ]
   show ?thesis
     unfolding A_def[symmetric] precond_dc_def
-    by (sep_auto simp: deadlock_checker.refine[symmetric] mod_star_conv)
+    by (sep_auto simp:
+        deadlock_checker.refine[symmetric] UPPAAL_Reachability_Problem_precompiled_defs.init_def 
+        mod_star_conv)
 qed
 
 export_code precond_dc checking SML
