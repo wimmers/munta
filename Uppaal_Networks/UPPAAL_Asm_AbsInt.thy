fcd8503844b706885fa93772df292cfed15b1eb5
Florian Märkl <info@florianmaerkl.de>
2020-06-01 22:31:25 +0200

    Dirty Collecting Semantics

--- Uppaal_Networks/UPPAAL_Asm_AbsInt.thy
+++ Uppaal_Networks/UPPAAL_Asm_AbsInt.thy
@@ -1,35 +1,63 @@
 theory UPPAAL_Asm_AbsInt
-  imports UPPAAL_Asm
+imports
+  UPPAAL_Asm
+  "HOL-Data_Structures.RBT_Map"
+  "HOL.List"
 begin
 
-type_synonym 'a ainstr = "instr * 'a" \<comment> \<open>Instruction with state after it was executed\<close>
-type_synonym 'a aprogram = "addr \<Rightarrow> (('a ainstr) option)"
+type_synonym 'a astate = "(addr * 'a) rbt"
+type_synonym cpstate = "stack * rstate * flag * nat list"
+type_synonym cstate = "cpstate list astate"
 
-datatype 'a spaced_aprogram = SpacedAProgram addrspace "'a aprogram"
+find_consts "'a \<Rightarrow> 'a option \<Rightarrow> 'a"
+find_consts "('a \<Rightarrow> 'b option) \<Rightarrow> 'a set \<Rightarrow> 'b set"
+find_consts "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list"
+find_consts "('a \<Rightarrow> 'b option) \<Rightarrow> 'a list \<Rightarrow> 'b list"
 
-fun annot_all :: "program \<Rightarrow> 'a \<Rightarrow> 'a aprogram" where
-  "annot_all prog v addr =
-    (case prog addr of
-        Some instr \<Rightarrow> Some (instr, v)
-      | None \<Rightarrow> None)"
+fun def :: "'a \<Rightarrow> 'a option \<Rightarrow> 'a" where
+  "def _ (Some v) = v" |
+  "def d _ = d"
 
-fun collect_predecs :: "addrspace \<Rightarrow> state set aprogram \<Rightarrow> addr \<Rightarrow> state set" where
-  "collect_predecs space prog pc =
-    \<Union> ((\<lambda>epc.
-          case prog pc of
-              Some (_, sts) \<Rightarrow> Set.filter (\<lambda>stt. state_pc stt = pc) sts
-            | None \<Rightarrow> {}
-      ) ` space)"
+fun keys :: "('a * 'b) rbt \<Rightarrow> 'a list" where
+  "keys t = map (\<lambda>(k, _) \<Rightarrow> k) (inorder t)"
 
-fun collect_step :: "addrspace \<Rightarrow> state set aprogram \<Rightarrow> state \<Rightarrow> state set aprogram" where
-  "collect_step space prog entry pc =
-    (case prog pc of
-        Some (instr, _) \<Rightarrow>
-          let instates = (collect_predecs space prog pc) \<union> (if state_pc entry = pc then {entry} else {});
-              outstates = List.map_project (\<lambda>ist. step instr ist) instates in
-          Some (instr, outstates)
-      | None \<Rightarrow> None)"
+(*fun insert :: "'a \<Rightarrow> 'b set \<Rightarrow> ('a::linorder * 'b set) rbt \<Rightarrow> ('a * 'b set) rbt" where
+  "insert k v t = update k
+    (case lookup t k of
+      Some vv \<Rightarrow> v \<union> vv |
+      None \<Rightarrow> v) t"*)
 
+(*fun merge :: "('a::linorder * 'b set) rbt \<Rightarrow> ('a * 'b set) rbt \<Rightarrow> ('a * 'b set) rbt" where
+  "merge a b = fold (\<lambda>(k, v). insert k v) (inorder b) a"*)
 
+fun insert :: "'a \<Rightarrow> 'b list \<Rightarrow> ('a::linorder * 'b list) rbt \<Rightarrow> ('a * 'b list) rbt" where
+  "insert k v t = update k
+    (case lookup t k of
+      Some vv \<Rightarrow> v @ vv |
+      None \<Rightarrow> v) t"
 
+fun merge :: "('a::linorder * 'b list) rbt \<Rightarrow> ('a * 'b list) rbt \<Rightarrow> ('a * 'b list) rbt" where
+  "merge a b = fold (\<lambda>(k, v). insert k v) (inorder b) a"
+
+fun step_all :: "instr \<Rightarrow> addr \<Rightarrow> cpstate list \<Rightarrow> state list" where
+  "step_all op pc instates =
+    List.map_filter (\<lambda>ins. step op (pc, ins)) instates"
+
+fun collect_step :: "program \<Rightarrow> cstate \<Rightarrow> cstate option" where
+  "collect_step prog st =
+    fold (\<lambda>pc ost.
+      case (ost, prog pc) of
+        (Some st, Some op) \<Rightarrow>
+          let ins = def [] (lookup st pc) in
+          let res = step_all op pc ins in
+          Some (fold (\<lambda>(pc, pst) st. insert pc [pst] st) res st)
+        | _ \<Rightarrow> None
+    ) (keys st) (Some st)"
+
+fun collect_loop :: "program \<Rightarrow> fuel \<Rightarrow> cstate \<Rightarrow> cstate option" where
+  "collect_loop prog 0 st = Some st" |
+  "collect_loop prog (Suc n) st =
+    (case collect_step prog st of
+      Some nst \<Rightarrow> collect_loop prog n (merge st nst) |
+      None \<Rightarrow> None)"
 end
--- Uppaal_Networks/UPPAAL_Asm_Test.thy
+++ Uppaal_Networks/UPPAAL_Asm_Test.thy
@@ -1,12 +1,14 @@
 theory UPPAAL_Asm_Test
-  imports UPPAAL_Asm_AbsInt
+  imports UPPAAL_Asm_AbsInt "HOL-IMP.AExp"
 begin
 
 definition myprog :: "instr list" where "myprog \<equiv> [
 PUSH 3,
 PUSH 42,
-LT
+instr.LT
 ]"
+
+definition empty_state :: "cpstate" where "empty_state = ([], [], False, [])"
 
 find_consts "('a \<Rightarrow> bool) \<Rightarrow> 'a set"
 
@@ -17,22 +19,91 @@ fun assemble :: "instr list \<Rightarrow> spaced_program" where
     (set (upt 0 (length listing)))
     (\<lambda>pc. if pc < length listing then Some (listing ! pc) else None)"
 
-class assembly =
-  fixes disassemble :: "'a \<Rightarrow> (addr * instr) list"
+fun spprog :: "spaced_program \<Rightarrow> program" where
+  "spprog (SpacedProgram _ prog) = prog"
 
-instantiation spaced_program :: assembly
+value "
+  let prog = spprog (assemble myprog) in
+  let entry = update (0::addr) [empty_state] empty in
+  collect_loop prog 2 entry"
+
+class display =
+  fixes display :: "'a \<Rightarrow> string"
+
+instantiation nat :: display
 begin
-fun disassemble_spaced_program :: "spaced_program \<Rightarrow> (addr * instr) list" where
-  "disassemble_spaced_program (SpacedProgram space prog) =
-    map (\<lambda>pc. (pc, the (prog pc))) (sorted_list_of_set space)"
-instance proof
-qed
+fun display_nat :: "nat \<Rightarrow> string" where
+  "display_nat n = ''nat''"
+instance proof qed
 end
 
-value "disassemble (assemble myprog)"
+instantiation int :: display
+begin
+fun display_int :: "int \<Rightarrow> string" where
+  "display_int n = ''int''"
+instance proof qed
+end
+
+instantiation bool :: display
+begin
+fun display_bool :: "bool \<Rightarrow> string" where
+  "display_bool True = ''True''" |
+  "display_bool False = ''False''"
+instance proof qed
+end
+
+instantiation instr :: display
+begin
+fun display_instr :: "instr \<Rightarrow> string" where
+  "display_instr (JMPZ a) = ''JMPZ '' @ (display a)" |
+  "display_instr ADD = ''ADD''" |
+  "display_instr NOT = ''NOT''" |
+  "display_instr AND = ''AND''" |
+  "display_instr instr.LT = ''LT''" |
+  "display_instr instr.LE = ''LE''" |
+  "display_instr instr.EQ = ''EQ''" |
+  "display_instr (PUSH imm) = ''PUSH '' @ (display imm)" |
+  "display_instr POP = ''POP''" |
+  "display_instr (LID r) = ''LID '' @ (display r)"|
+  "display_instr STORE = ''STORE''" |
+  "display_instr (STOREI r imm) = ''STOREI '' @ (display r) @ '' '' @ (display imm)"|
+  "display_instr COPY = ''COPY''" |
+  "display_instr CALL = ''CALL''" |
+  "display_instr RETURN = ''RETURN''" |
+  "display_instr HALT = ''HALT''" |
+  "display_instr (STOREC n i) = ''STOREC '' @ (display n) @ '' '' @ (display i)" |
+  "display_instr (SETF b) = ''SETF '' @ (display b)"
+instance proof qed
+end
+
+instantiation spaced_program :: display
+begin
+fun display_spaced_program :: "spaced_program \<Rightarrow> string" where
+  "display_spaced_program (SpacedProgram space prog) =
+    concat (map (\<lambda>pc. (display pc) @ '' '' @ (display (the (prog pc))) @ [char_of (10::nat)]) (sorted_list_of_set space))"
+instance proof qed
+
+definition make_string :: "spaced_program \<Rightarrow> string" where
+  "make_string p \<equiv> display p"
+
+value "display (SpacedProgram {0, 1, 2} (\<lambda>a. Some (myprog ! a)))"
+
+typ "spaced_program"
 
 abbreviation init where "init \<equiv> (0, [], [], False, [])"
 
 value "case assemble myprog of (SpacedProgram space prog) \<Rightarrow> step (the (prog (state_pc init))) init"
+
+(*theory Scratch
+imports \<comment> \<open>HOL.Archimedean_Field "HOL-Library.Stream"\<close> "HOL-IMP.AExp"
+begin*)
+
+value "(\<lambda>_::nat. 3::nat)(5:= 4)"
+value "<''y'' := 7::nat>"
+value [simp] "map (<''y'' := 7::nat>) [''x'', ''y'']"
+definition "my_string \<equiv> STR ''a \<newline> b''"
+ML \<open>val str = @{code my_string}
+val _ = writeln str
+\<close>
 
 end
