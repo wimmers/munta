77e8c399db4a1d40996b00efc8015f901af3eb10
Simon Wimmer <swimmer@cs.au.dk>
2022-02-07 09:05:20 +0100

    Abstract let: refactor & add safety guard

--- library/Abstract_Term.thy
+++ library/Abstract_Term.thy
@@ -3,25 +3,29 @@ theory Abstract_Term
 begin
 
 ML \<open>
-  fun pull_let ctxt u name t =
+  fun pull_let ctxt target name body =
     let
-      val t1 = abstract_over (u, t);
-      val r1 = Const (@{const_name "HOL.Let"}, dummyT) $ u $ Abs (name, dummyT, t1);
+      val body1 = abstract_over (target, body);
+      val _ =
+        if body aconv body1 then
+          raise (TERM ("could not pull term; type mismatch?", [target, body]))
+        else ()
+      val r1 = Const (\<^const_name>\<open>HOL.Let\<close>, dummyT) $ target $ Abs (name, dummyT, body1);
       val ct1 = Syntax.check_term ctxt r1;
       val g1 =
-        Goal.prove ctxt [] [] (Logic.mk_equals (t, ct1))
+        Goal.prove ctxt [] [] (Logic.mk_equals (body, ct1))
         (fn {context, ...} => EqSubst.eqsubst_tac context [0] [@{thm Let_def}] 1
         THEN resolve_tac context [@{thm Pure.reflexive}] 1)
     in g1 end;
 
   fun get_rhs thm =
     let
-      val Const ("Pure.eq", _) $ _ $ r = Thm.full_prop_of thm
+      val \<^Const>\<open>Pure.eq _ for _ r\<close> = Thm.full_prop_of thm
     in r end;
 
   fun get_lhs thm =
     let
-      val Const ("Pure.imp", _) $ (Const ("Pure.eq", _) $ l $ _) $ _ = Thm.full_prop_of thm
+      val \<^Const>\<open>Pure.imp for \<^Const>\<open>Pure.eq _ for l _\<close> _\<close> = Thm.full_prop_of thm
     in l end;
 
   fun pull_tac' ctxt u name thm =
@@ -34,7 +38,7 @@ ML \<open>
 \<close>
 
 ML \<open>
-fun lift_parser p = fn (ctxt, x) => Args.text x |> (fn (r, s) => (r, (ctxt, s)))
+fun lift_parser p = fn (ctxt, x) => p x |> (fn (r, s) => (r, (ctxt, s)))
 \<close>
 
 method_setup abstract_let =
@@ -46,6 +50,7 @@ method_setup abstract_let =
 text \<open>Example Usage\<close>
 
 schematic_goal "(1 :: nat) * 2 * 3 \<equiv> ?x"
+  \<^cancel>\<open>apply (abstract_let 1)\<close> \<comment> \<open>Results in error: types do not match\<close>
   apply (abstract_let "1 :: nat" one)
   apply (abstract_let "2 :: nat")
   apply (abstract_let "3 :: nat" two)
